5299e52216cf0ebc71ef63120e73287aa018a2de
jasonarrage <117213951+jasonarrage@users.noreply.github.com>
2022-11-01 19:49:40 -0400

    Add files via upload

---
+++ print_hello_world.py
@@ -0,0 +1,7 @@
+#this is a calculator. it asks the user for the two numbers and an operator and evaluates.
+
+chiffre1=input("quel est le premier chiffre?: ")
+operateur=input("quel est l'operateur?: ")
+chiffre2=input("quel est le deuxieme chiffre?: ")
+calcul=eval(chiffre1+operateur+chiffre2)
+print(calcul)
